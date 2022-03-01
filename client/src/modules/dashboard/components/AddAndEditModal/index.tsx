a4a24b1c17d307960d71be7856eb1dea76200447
Jane Alam Tusher <jaanealamtusher@gmail.com>
2022-03-02 00:54:47 +0600

    Modal design

---
+++ client/src/modules/dashboard/components/AddAndEditModal/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Modal from '../../../common/components/Modal';
+import TextInput from '../../../common/components/TextInput';
+import SingleSelect from '../../../common/components/SingleSelect';
+import Button from '../../../common/components/Button';
+
+interface IEditAndAddModal {
+    onClose: () => void;
+    open: boolean;
+}
+
+const EditAndAddModal: React.FC<IEditAndAddModal> = ({ onClose, open }) => {
+
+    
+    return (
+        <Modal onClose={onClose} open={open}>
+            <div className='pt-10 pb-20'>
+                <h2>
+                    <h2 className='mb-8 text-center'>Add new Member</h2>
+                </h2>
+                <div className=' max-w-3xl w-full mx-auto px-5'>
+                    <div className='mb-10 flex gap-5'>
+                        <TextInput className='w-6/12' label='State' />
+                        <TextInput className='w-6/12' label='Zip code' />
+                    </div>
+                    <div className='mb-10 flex gap-5'>
+                        <SingleSelect
+                            className='w-6/12'
+                            data={{ value: '', label: '' }}
+                            items={[]}
+                            label='Tusher'
+                            onChange={() => console.log('Tusher')}
+                        />
+                        <TextInput className='w-6/12' label='City' />
+                    </div>
+                    <div className='mb-10 flex gap-5'>
+                        <TextInput className='w-6/12' label='State' />
+                        <TextInput className='w-6/12' label='Zip code' />
+                    </div>
+                    <div className='flex justify-end pb-20'>
+                        <Button>Add Address</Button>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    );
+};
+
+export default EditAndAddModal;
--- client/src/modules/dashboard/components/MemberLists/ListBar.tsx
+++ client/src/modules/dashboard/components/MemberLists/ListBar.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../../common/components/Button';
 import useScrollPosition from '../../../common/hooks/useScrollPosition';
+import EditAndAddModal from '../AddAndEditModal';
+
 const ListBar = () => {
+    const [addModalOpen, setAddModalOpen] = useState(false);
     const { ref, position } = useScrollPosition();
     return (
         <React.Fragment>
@@ -13,13 +16,16 @@ const ListBar = () => {
             <div
                 ref={ref}
                 className={`top-24 transition-all duration-300 py-3 sticky mt-0.5 ${
-                    position.top !== undefined && position.top <= 96 ? 'bg-white z-50' : ''
+                    position.top !== undefined && position.top <= 96 ? 'bg-white z-50 shadow' : ''
                 }`}
             >
                 <div className='max-w-6xl mb-2 relative px-5 mx-auto flex justify-end'>
-                    <Button className="transition" color={position.top !== undefined && position.top <= 96 ? 'primary' : 'secondary'}>Add new</Button>
+                    <Button onClick={()=> setAddModalOpen(true)} className='transition' color={position.top !== undefined && position.top <= 96 ? 'primary' : 'secondary'}>
+                        Add new
+                    </Button>
                 </div>
             </div>
+            <EditAndAddModal open={addModalOpen} onClose={() => setAddModalOpen(false)} />
         </React.Fragment>
     );
 };
